20a8b2446b9b3143c88c867cc3ccd46ecf1d9c78
Muhammed Ali SUSLU <malisuslu@yandex.com>
2022-05-03 19:10:54 +0300

    "Some improvements are applied"

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-
-
-function roll_the_dices() { document.querySelector(".btn").addEventListener("click", function() {
+function roll_the_dices() { 
     
     let all_circles = document.querySelectorAll(".circle");
     console.log(all_circles);
@@ -9,9 +7,48 @@ function roll_the_dices() { document.querySelector(".btn").addEventListener("cli
         all_circles[i].style.backgroundColor = "hsl(0, 56%, 49%)";
     }
 
-    let player1 = Math.floor(Math.random() * 6) + 1;
-    let player2 = Math.floor(Math.random() * 6) + 1;
+    player1 = Math.floor(Math.random() * 6) + 1;
+    player2 = Math.floor(Math.random() * 6) + 1;
 
+    let circle1 =document.querySelectorAll(`#p1 .circle`);
+    let circle2 =document.querySelectorAll("#p2 .circle");
+
+    function colorDot(arr, circle) { 
+        return arr.map(x => circle[x]).map(x => x.style.backgroundColor = "white");
+     }
+
+    function diceFace(player, circle) {
+        switch (player) {
+            case 1:
+                colorDot([4], circle);
+                break;
+            case 2:
+                colorDot([0,8], circle);
+                break;
+            case 3:
+                colorDot([0,4,8], circle);
+                break;
+            case 4:
+                colorDot([0,2,6,8], circle);
+                break;
+            case 5:
+                colorDot([0,2,4,6,8], circle);
+                break;
+            case 6:
+                colorDot([0,1,2,6,7,8], circle);
+                break;
+        }
+     }
+
+     diceFace(player1, circle1);
+     diceFace(player2, circle2);
+     
+};
+
+let player1 = 0;
+let player2 = 0;
+
+function winner() {
     if (player1 > player2) {
         document.querySelector("h1").innerHTML = "<img src='red-flag.png' alt='red_flag'> Player 1 Wins!";
     } else if (player2 > player1) {
@@ -19,53 +56,14 @@ function roll_the_dices() { document.querySelector(".btn").addEventListener("cli
     } else {
         document.querySelector("h1").innerHTML = "Draw!";
     }
-
-    let circle1 =document.querySelectorAll("#p1 .circle");
-    let circle2 =document.querySelectorAll("#p2 .circle");
-
-    switch (player1) {
-        case 1:
-            [4].map(x => circle1[x]).map(x => x.style.backgroundColor = "white");
-        break;
-        case 2:
-            [0,8].map(x => circle1[x]).map(x => x.style.backgroundColor = "white");
-        break;
-        case 3:
-            [0,4,8].map(x => circle1[x]).map(x => x.style.backgroundColor = "white");
-        break;
-        case 4:
-            [0,2,6,8].map(x => circle1[x]).map(x => x.style.backgroundColor = "white");
-        break;
-        case 5:
-            [0,2,4,6,8].map(x => circle1[x]).map(x => x.style.backgroundColor = "white");
-        break;
-        case 6:
-            [0,1,2,6,7,8].map(x => circle1[x]).map(x => x.style.backgroundColor = "white");
-        break;
-    }
-
-
-    switch (player2) {
-        case 1:
-            [4].map(x => circle2[x]).map(x => x.style.backgroundColor = "white");
-        break;
-        case 2:
-            [0,8].map(x => circle2[x]).map(x => x.style.backgroundColor = "white");
-        break;
-        case 3:
-            [0,4,8].map(x => circle2[x]).map(x => x.style.backgroundColor = "white");
-        break;
-        case 4:
-            [0,2,6,8].map(x => circle2[x]).map(x => x.style.backgroundColor = "white");
-        break;
-        case 5:
-            [0,2,4,6,8].map(x => circle2[x]).map(x => x.style.backgroundColor = "white");
-        break;
-        case 6:
-            [0,1,2,6,7,8].map(x => circle2[x]).map(x => x.style.backgroundColor = "white");
-        break;
-    }
-});
 }
 
-roll_the_dices();
+function throw_the_dices() {
+    
+    for (let i = 0; i < 20; i++) {
+        setTimeout(roll_the_dices, 100 * i);
+    };
+    setTimeout(winner, 100 * 20+50);
+}
+
+document.querySelector(".button-73").addEventListener("click", throw_the_dices);
